3714cad7e5fbcf3e902c2b9514691c1c5fa20735
SergeiiKalina <117165743+SergeiiKalina@users.noreply.github.com>
2024-09-02 13:56:32 +0300

    delete comments

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -4,27 +4,23 @@ import { locales, loadTranslations, translations, defaultLocale } from '$lib/dic
 export const load = async ({ url, cookies, request }) => {
 	const { pathname } = url;
 
-	// Try to get the locale from cookie
 	let locale = (cookies.get('lang') || '').toLowerCase();
 
-	// Get user preferred locale
 	if (!locale) {
 		locale =
 			`${`${request.headers.get('accept-language')}`.match(/[a-zA-Z]+?(?=-|_|,|;)/)}`.toLowerCase();
 	}
 
-	// Get defined locales
 	const supportedLocales = locales.get().map((l) => l.toLowerCase());
 
-	// Use default locale if current locale is not supported
 	if (!supportedLocales.includes(locale)) {
 		locale = defaultLocale;
 	}
 
-	await loadTranslations(locale, pathname); // keep this just before the `return`
+	await loadTranslations(locale, pathname); 
 
 	return {
 		i18n: { locale, route: pathname },
-		translations: translations.get() // `translations` on server contain all translations loaded by different clients
+		translations: translations.get() 
 	};
 };
