ff0576f360938e814d56aface20489bda963342a
Serhii Kalyna <setserg02@gmail.com>
2024-09-11 09:23:45 +0300

    add blogs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
 <script>
 	import { t } from '$lib/dictionary/index.js';
+	import blogs from '$lib/blogs/articlesUa.json'
+
+	const sendBlogs = async () => {
+		for(let i = 0; i < blogs.length; i++){
+			
+			await fetch("http://localhost:3000/blogs", {
+				method: 'POST',
+				headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(blogs[i])
+			})
+		}
+const res = await fetch("http://localhost:3000/blogs")
+console.log(res)
+	}
 </script>
 
 <div>{$t('home.home_content')}</div>
+
+<button on:click={sendBlogs}>send</button>
 
 <style>
 	div {
