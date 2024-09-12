dd273a63a83af3fc7238979adf8ff2bdfa18bd5b
Serhii Kalyna <setserg02@gmail.com>
2024-09-12 13:35:59 +0300

    update functional add blogs

--- src/routes/add-blog/+page.svelte
+++ src/routes/add-blog/+page.svelte
@@ -1,31 +1,63 @@
 <script>
 	import { uploadPhoto } from './store.js';
-	let hex = '#000000';
+	import { LoremIpsum } from 'lorem-ipsum';
+	const lorem = new LoremIpsum({
+		sentencesPerParagraph: {
+			max: 8,
+			min: 4
+		},
+		wordsPerSentence: {
+			max: 16,
+			min: 4
+		}
+	});
+
+	let currentItem = null;
 	let title = '';
+	let paragraph = '';
 	let selectionStart = 0;
 	let selectionEnd = 0;
 	let toggleImgPopup = false;
 	let toggleAPopup = false;
+	let togglePPopup = false;
+	let toggleListPopup = false;
+	let inputList = ['item-1'];
 	let imgSrc = '';
 	let aHref = '';
-	$: value = `<section>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus aperiam adipisci, dolorem vitae, necessitatibus commodi similique eaque illum labore fugit sed eveniet. Illum praesentium nobis ea, ex recusandae officia eveniet!</section>`;
+	$: value = [
+		`<p on:touchstart={handlerTouchStart} draggable style="cursor: pointer;">${lorem.generateParagraphs(2)}</p>`
+	];
 	$: text = removeHtmlTags(value);
 	$: visibleTitle = `<h2>${title}</h2>`;
 
+	let draggedIndex;
+
+	function handleDragStart(e, index) {
+		draggedIndex = index;
+		e.dataTransfer.effectAllowed = 'move';
+	}
+
+	function handleDrop(e, index) {
+		e.preventDefault();
+		if (draggedIndex !== index) {
+			const draggedItem = value[draggedIndex];
+			value = value.filter((_, i) => i !== draggedIndex);
+			value = [...value.slice(0, index), draggedItem, ...value.slice(index)];
+		}
+	}
+
 	function removeHtmlTags(html) {
-		return html.replace(/<[^>]*>/g, '');
+		return html.join('').replace(/<[^>]*>/g, '');
 	}
 
 	const addP = () => {
-		if (selectionStart === selectionEnd) return;
-
-		const selectionText = value.substring(selectionStart, selectionEnd);
-		const selectionTextWithTag = `<p>${selectionText}</p>`;
-		value =
-			value.substring(0, selectionStart) + selectionTextWithTag + value.substring(selectionEnd);
+		const pTextArea = document.querySelector('textarea');
+		value = [...value, `<p>${pTextArea.value}</p>`];
+		togglePPopup = false;
 	};
 
-	const addA = () => {
+	const addA = (currentItem) => {
+		console.log(currentItem);
 		if (selectionStart === selectionEnd) return;
 		toggleAPopup = true;
 	};
@@ -36,67 +68,127 @@
 		selectionEnd = textarea.selectionEnd;
 	};
 
-	const handlerWriteText = (e) => {
-		const target = e.target;
-		if (value.length === 0) {
-			value = `${target.value}`;
-		} else {
-			value = target.value;
+	const addList = (e) => {
+		const listArr = [];
+		const form = new FormData(e.target);
+		for (const [key, value] of form.entries()) {
+			listArr.push(value);
 		}
+		value = [...value, `<ul>${listArr.map((el) => `<li>${el}</li>`).join('')}</ul>`];
+		toggleListPopup = false;
 	};
 
-	const handlePopup = (e) => {
-		if (e.target.id !== 'imgForm') togglePopup = false;
-	};
-
-	const changeTag = (e) => {
-		console.log(e);
-	};
+	function handleTextSelection(e) {
+		const selection = window.getSelection();
+		if (selection.rangeCount > 0) {
+			const selectedText = selection.toString();
+			console.log('Selected text:', selectedText);
+		}
+	}
 </script>
 
 <div class="container">
-	<form>
-		<section class="input_block">
-			<label>Title<input type="text" name="title" bind:value={title} /></label>
-			<label>Title for seo<input type="text" name="titleseo" /></label>
-			<label>Description for seo<input type="text" name="descriptionseo" /></label>
-			<label>Slug<input type="text" name="slug" /></label>
-		</section>
-		<h3>Content</h3>
-		<section class="style_block">
-			<button type="button" on:click={addP}>P</button>
-			<button type="button" on:click={addA}>A</button>
-			<button
-				type="button"
-				id="img"
-				on:click={() => {
-					toggleImgPopup = true;
-				}}>add image</button
-			><button
-				on:click={() => {
-					if (selectionStart === selectionEnd) return;
-					const selectionText = value.substring(selectionStart, selectionEnd);
-					const selectionTextWithTag = `<span style="color: red">${selectionText}</span>`;
-					value =
-						value.substring(0, selectionStart) +
-						selectionTextWithTag +
-						value.substring(selectionEnd);
-				}}>red</button
-			>
-		</section>
-
-		<textarea name="text" rows="30" on:select={handlerSelectTextarea} on:change={handlerWriteText}
-			>{value}</textarea
+	<section class="block-add-blocks">
+		<button
+			on:click={() => {
+				togglePPopup = true;
+			}}>Add paragraph</button
 		>
-		<select>
-			<option value="article">Article</option>
-		</select>
-	</form>
+		<button
+			on:click={() => {
+				toggleListPopup = true;
+			}}>Add list</button
+		>
+		<button
+			on:click={() => {
+				toggleImgPopup = true;
+			}}>Add image</button
+		>
+	</section>
 	<section class="preview_block">
 		{#if title}<div>{@html visibleTitle}</div>{/if}
-		<div>{@html value}</div>
+		<div role="list">
+			{#each value as item, index}
+				<div
+					class="draggable-block"
+					role="listitem"
+					draggable="true"
+					on:dragstart={(e) => handleDragStart(e, index)}
+					on:drop={(e) => handleDrop(e, index)}
+					on:dragover={(e) => e.preventDefault()}
+					aria-grabbed={index === draggedIndex}
+				>
+					<button
+						on:click={(e) => {
+							const block = e.target.closest('.draggable-block');
+							block.draggable = false;
+						}}>edit</button
+					>
+					<button
+						on:click={(e) => {
+							const block = e.target.closest('.draggable-block');
+							currentItem = block;
+							addA(currentItem);
+							// block.draggable = true;
+						}}>add link</button
+					>
+					{@html item}
+				</div>
+			{/each}
+		</div>
 	</section>
 </div>
+{#if toggleListPopup}<div class="pop-up">
+		<div class="p-popup">
+			<button
+				class="a-popup-close"
+				type="button"
+				on:click={() => {
+					toggleListPopup = false;
+				}}>X</button
+			>
+			<h2>Add List</h2>
+			<button
+				on:click={() => {
+					inputList = [...inputList, `item-${inputList.length + 1}`];
+				}}>+</button
+			>
+			<form class="block-list" on:submit={addList}>
+				{#each inputList as item}
+					<label>{item}<input type="text" name={item} /></label>
+				{/each}
+
+				<button type="submit"> ADD All list</button>
+			</form>
+		</div>
+	</div>{/if}
+{#if togglePPopup}<div class="pop-up">
+		<div class="p-popup">
+			<button
+				class="a-popup-close"
+				type="button"
+				on:click={() => {
+					togglePPopup = false;
+				}}>X</button
+			>
+			<h2>Add Paragraph</h2>
+			<button
+				on:click={() => {
+					paragraph = lorem.generateParagraphs(2);
+				}}
+			>
+				generate paragraph</button
+			>
+			<textarea name="paragraph" rows="20" cols="60" bind:value={paragraph}></textarea>
+			<button
+				on:click={() => {
+					addP();
+				}}
+			>
+				ADD</button
+			>
+		</div>
+	</div>{/if}
 {#if toggleImgPopup}<div class="pop-up">
 		<div class="input-img" id="imgForm">
 			<input
@@ -108,7 +200,10 @@
 			/>
 			<button
 				on:click={() => {
-					value = value + `<div><img width="100%" src="${imgSrc}" alt="pictures"/></div>`;
+					value = [
+						...value,
+						`<div style="margin-top: 24px;"><img width="100%" src="${imgSrc}" alt="pictures"/></div>`
+					];
 					toggleImgPopup = false;
 				}}>ADD</button
 			>
@@ -148,6 +243,34 @@
 {/if}
 
 <style>
+	div[role='listitem'] {
+		position: relative;
+		border: 1px solid #ccc;
+		padding: 8px;
+		margin: 4px 0;
+		cursor: move;
+	}
+	.block-list {
+		display: flex;
+		flex-direction: column;
+	}
+	.block-add-blocks button {
+		background-color: indigo;
+		width: 150px;
+		height: 30px;
+		border-radius: 12px;
+	}
+	.p-popup {
+		position: relative;
+		width: 50%;
+		height: 50%;
+		background-color: rgb(251, 255, 0);
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		border-radius: 20px;
+	}
 	.a-popup-close {
 		position: absolute;
 		top: 10px;
@@ -170,11 +293,7 @@
 		align-items: center;
 		border-radius: 20px;
 	}
-	.style_block {
-		display: flex;
-		width: 70%;
-		justify-content: space-evenly;
-	}
+
 	.input-img {
 		position: relative;
 		width: 30%;
@@ -201,50 +320,14 @@
 		align-items: center;
 		justify-content: center;
 		flex-direction: column;
-		max-width: 30%;
+		max-width: 75%;
 		height: 100%;
 	}
-
-	form {
-		width: 50%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: center;
-		height: 700px;
-	}
-	.input_block {
-		width: 50%;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-around;
-		align-items: center;
-		height: 300px;
-	}
-	textarea {
-		width: 500px;
-		margin: 12px auto 0 auto;
-	}
-
-	input {
-		width: 200px;
-		height: 24px;
-	}
-	label {
-		width: 100%;
-		display: flex;
-		justify-content: space-between;
-	}
-
 	.container {
 		display: flex;
+		flex-direction: column;
 		justify-content: center;
 		align-items: center;
-		height: 75vh;
-	}
-	select {
-		width: 120px;
-		height: 30px;
-		margin-top: 24px;
+		margin-top: 36px;
 	}
 </style>
